b2822edbde272177b342b95b150795a888bd92f5
Mateo <mateodioev@noemail.com>
2022-10-19 18:40:44 -0500

    support multiple Files

--- src/Types/InputFile.php
+++ src/Types/InputFile.php
@@ -11,11 +11,17 @@ use function basename;
 class InputFile implements TypesInterface
 {
   public CURLFile|string $file;
+  public array $files;
+
 
   public function __construct($file) {
     $this->file = $file;
+    $this->files[] = $this;
   }
 
+  /**
+   * Upload local file
+   */
   public static function fromLocal(string $filePath, ?string $fileName = ''): InputFile
   {
     if (!Files::isFile($filePath)) {
@@ -26,13 +32,39 @@ class InputFile implements TypesInterface
     return new self($file);
   }
 
-  public function fromUrl(string $url): InputFile
+  /**
+   * Send external file
+   */
+  public static function fromUrl(string $url): InputFile
   {
     if (!Network::IsValidUrl($url)) {
       throw new InvalidFileException('Invalid URL');
     }
 
     return new self($url);
+  }
+
+  /**
+   * Send a file already exists in telegram servers
+   */
+  public static function fromId(string $id): InputFile
+  {
+    if (empty($id)) {
+      throw new InvalidFileException('File id cant not be empty');
+    }
+
+    return new self($id);
+  }
+
+  public function addNewFile(InputFile $file): InputFile
+  {
+    $this->files[] = $file;
+    return $this;
+  }
+
+  public function getFiles(): array
+  {
+    return $this->files;
   }
 
   public function get()
