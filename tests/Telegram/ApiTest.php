d2f99505cfce9ee23deece1787efb9a3a79542c7
Mateo <orehurcomito@gmail.com>
2022-11-28 11:36:14 -0500

    add tests

---
+++ tests/Telegram/ApiTest.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace Tests\Telegram;
+
+use Mateodioev\Bots\Telegram\Api;
+use Mateodioev\Bots\Telegram\Config\Types as ApiConfig;
+use Mateodioev\Bots\Telegram\Exception\TelegramApiException;
+use Mateodioev\Bots\Telegram\Methods\Method;
+use Mateodioev\Bots\Telegram\Types\{Error, User};
+use PHPUnit\Framework\TestCase;
+
+class ApiTest extends TestCase
+{
+  private static Api $api;
+
+  public static function setUpBeforeClass(): void
+  {
+    self::$api = Api::fromEnv();
+  }
+
+  private function getInvalidMethod(): Method
+  {
+    return Method::create(method: 'MyInvalidMethod')
+      ->setReturnType(Error::class);
+  }
+
+  public function testGetMe()
+  {
+    $this->assertInstanceOf(User::class, self::$api->getMe());
+  }
+
+  public function testAssertTelegramApiException()
+  {
+    $this->expectException(TelegramApiException::class);
+    self::$api->request($this->getInvalidMethod());
+  }
+
+  public function testIgnoreExceptionAndAssertError()
+  {
+    ApiConfig::setThrowExceptionOnFail(false);
+
+    $res = self::$api->request($this->getInvalidMethod());
+    $this->assertInstanceOf(Error::class, $res);
+  }
+}
